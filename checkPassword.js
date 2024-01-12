3848a960d164671ba91815dda4655b27c908447a
mkatay <kmagdi@kkando.hu>
2024-01-12 11:48:32 +0100

    change myfunction

--- checkPassword.js
+++ checkPassword.js
@@ -1,4 +1,5 @@
-//a passwornak min 6 hosszuságúkell legyen, legyen benne kis és nagy betű és szám is:
+//a passwornak min 6 hosszuságú kell legyen
+// legyen benne kis és nagy betű és szám is!
 function allCases(string) {
     const
         upper = /[A-Z]/.test(string),
@@ -10,11 +11,10 @@ function containNumber(string){
 }
 
 export const checkPassword=(pw)=>{
-    if(!pw) return false//utólag ide előre kell emelni
+    if(!pw) return false
     if(pw.length<6) return false
-    //  if(!pw) return false //így hibás a fg.,a 3.teszt nem fut le
     if(!allCases(pw)) return false
-    if(!containNumber) return false
+    if(!containNumber(pw)) return false
     return true
 
 }
